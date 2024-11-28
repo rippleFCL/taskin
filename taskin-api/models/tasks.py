a1abb712f48ff707eeb689eeee30f0d8a18b08d0
ripple <github@ripple.contact>
2024-11-28 03:40:34 +0000

    fixed incorrect models

--- taskin-api/models/tasks.py
+++ taskin-api/models/tasks.py
@@ -11,15 +11,15 @@ class StatusEnum(str, Enum):
 
 
 class Category(SQLModel, table=True):
-    id: UUID = Field(default_factory=uuid4, primary_key=True, index=True)
+    id: int | None = Field(default=None, primary_key=True, index=True)
     name: str = Field(index=True)
 
     tasks: list["Task"] = Relationship(back_populates="category")
 
 
 class Task(SQLModel, table=True):
-    id: UUID = Field(default_factory=uuid4, primary_key=True, index=True)
+    id: int | None = Field(default=None, primary_key=True, index=True)
     name: str = Field(index=True)
     status: StatusEnum = Field(default=StatusEnum.todo, index=True)
-    category_id: UUID = Field(default=None, foreign_key="category.id")
+    category_id: int | None = Field(default=None, foreign_key="category.id")
     category: Category = Relationship(back_populates="tasks")
