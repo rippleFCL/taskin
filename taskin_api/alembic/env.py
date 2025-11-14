08ee1298956a925c881e877a118e2cc3eb33c8d4
ripple <github@ripple.contact>
2025-11-14 07:12:52 +0000

    fix: logging config

--- taskin_api/alembic/env.py
+++ taskin_api/alembic/env.py
@@ -15,8 +15,9 @@ config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+# Disable to prevent overwriting application logging configuration
+# if config.config_file_name is not None:
+#     fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
